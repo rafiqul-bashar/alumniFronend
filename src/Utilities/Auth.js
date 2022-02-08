9383d09174dec392e5d82bc1adbb7e2d49449490
rafiqul-bashar <rafiqul.bashar@outlook.com>
2022-02-08 23:27:35 +0600

    makeItBetter

--- src/Utilities/Auth.js
+++ src/Utilities/Auth.js
@@ -1,48 +1,59 @@
+import { useToast } from '@chakra-ui/react';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import userApi from './userApi';
 
-
 const Auth = () => {
     const [admin, setAdmin] = useState(false);
-    const [isLoading, setIsLoading] = useState(false)
+    const [loadingState, setLoadingState] = useState(false)
     const navigate = useNavigate()
-    
+    const toast = useToast()
+    const { reset } = useForm()
     // (getSessionStorageOrDefault('user', ''))
     // function getSessionStorageOrDefault(key, defaultValue) {
-        //     const stored = sessionStorage.getItem(key);
-        //     if (!stored) {
+    //     const stored = sessionStorage.getItem(key);
+    //     if (!stored) {
     //         return defaultValue;
     //     }
     //     return JSON.parse(stored);
     // }
 
     //Register User Data
-    const getUserData = (key,defaultValue) => {
+    const getUserData = (key, defaultValue) => {
         let data = sessionStorage.getItem(key)
         data = JSON.parse(data)
-        if(data){
+        if (data) {
             return data;
         }
         return defaultValue
     }
-    const [user, setUser] = useState(getUserData('user',{}))
+    const [user, setUser] = useState(getUserData('user', {}))
 
     const registerUser = async (data) => {
         try {
             const newUser = { name: data.name, email: data.email, phone: data.phone, address: data.address };
             let res = await userApi.post("/users", newUser);
-            console.log(res.data);
-
+            if (res.data) { reset() };
         } catch (error) {
             // window.location.reload();
-            console.log(error);
+            console.log(error)
+        } finally {
+            toast({
+                title: 'Account created Successfully.',
+                description: "We've created your account for you. Login to continue",
+                status: 'success',
+                duration: 9000,
+                isClosable: true,
+            })
+            navigate('/login')
         }
     };
 
     //Get User Data from DB on Login
 
     const getUser = async (email) => {
+        setLoadingState(true)
         try {
             const res = await userApi.get(`/users/${email}`);
             setUser(res.data)
@@ -50,10 +61,12 @@ const Auth = () => {
             if (res.data.isAdmin === true) {
                 setAdmin(true);
             }
-            navigate('/home')
         } catch (error) {
             window.location.reload();
             console.log(error);
+        } finally {
+            setLoadingState(false)
+            navigate('/')
         }
     };
     const logOut = () => {
@@ -61,7 +74,7 @@ const Auth = () => {
         sessionStorage.clear()
 
     }
-    return { user, admin, getUser, registerUser, logOut }
+    return { user, admin, getUser, registerUser, logOut, loadingState }
 };
 
 export default Auth;
